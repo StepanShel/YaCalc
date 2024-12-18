abf905aab5869a481f63ee2a7b95355afaa03347
Stepan <St.shelkovnikov@ya.ru>
2024-12-18 22:04:41 +0300

    calc and calc_tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/StepanShel/YaCalc
+
+go 1.23.1
---
+++ pkg/calculation/Calc_test.go
@@ -0,0 +1,101 @@
+package calculation_test
+
+import (
+	"testing"
+
+	"github.com/StepanShel/YaCalc/pkg/calculation"
+)
+
+func TestCalc(t *testing.T) {
+	testCasesSuccess := []struct {
+		name           string
+		expression     string
+		expectedResult float64
+	}{
+		{
+			name:           "simple",
+			expression:     "1+1",
+			expectedResult: 2,
+		},
+		{
+			name:           "priority",
+			expression:     "(2+2)*2",
+			expectedResult: 8,
+		},
+		{
+			name:           "priority",
+			expression:     "2+2*2",
+			expectedResult: 6,
+		},
+		{
+			name:           "/",
+			expression:     "1/2",
+			expectedResult: 0.5,
+		},
+		{
+			name:           "brakcets",
+			expression:     "((1+4) * (1+2) +10) *4",
+			expectedResult: 100,
+		},
+		{
+			name:           "-",
+			expression:     "1299 - 299",
+			expectedResult: 1000,
+		},
+	}
+
+	for _, testCase := range testCasesSuccess {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := calculation.Calc(testCase.expression)
+			if err != nil {
+				t.Fatalf("successful case %s returns error", testCase.expression)
+			}
+			if val != testCase.expectedResult {
+				t.Fatalf("%f should be equal %f", val, testCase.expectedResult)
+			}
+		})
+	}
+
+	testCasesFail := []struct {
+		name        string
+		expression  string
+		expectedErr error
+	}{
+		{
+			name:       "simple",
+			expression: "1+1*",
+		},
+		{
+			name:       "priority",
+			expression: "2+2**2",
+		},
+		{
+			name:       "priority",
+			expression: "((2+2-*(2",
+		},
+		{
+			name:       "empty",
+			expression: "",
+		},
+		{
+			name:       "division by zero",
+			expression: "1/0",
+		},
+		{
+			name:       "brackets",
+			expression: "((1+4) * (1+2 +10) *4",
+		}, {
+			name:       "brackets",
+			expression: "1) + 2",
+		},
+	}
+
+	for _, testCase := range testCasesFail {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := calculation.Calc(testCase.expression)
+			if err == nil {
+				t.Fatalf("expression %s is invalid but result  %f was obtained", testCase.expression, val)
+			}
+		})
+	}
+}
